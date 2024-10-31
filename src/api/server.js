b83ef33fef39dd735971235d3c88a894356e862c
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-10-31 18:23:30 -0400

    Fixed login and signup so that they search correctly.

--- src/api/server.js
+++ src/api/server.js
@@ -1,10 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config();
 //const express = require('express');//Enables express
 //const bodyParser = require('body-parser');//Body parser for incoming request. May not be needed
 //const cors = require('cors');//Enables CORS secuirty handleing 
 //const path = require('path');
-
 
 dotenv.config({ path: '../../.env' })
 
@@ -19,154 +17,165 @@ const prisma = new PrismaClient()
 
 //Connect to database
 const client = new MongoClient(process.env.DATABASE_URL);
-//const prisma = new PrismaClient(process.env.DATABASE_URL) 
+// const prisma = new PrismaClient(process.env.DATABASE_URL)
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.set('port', (PORT));
-
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.set('port', PORT)
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-    );
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  )
+  next()
+})
 
 app.get('/', (req, res) => {
-    res.send('Server Is Ready');
+  res.send('Server Is Ready')
 })
 
 app.listen(PORT, () => {
-    console.log('Server listening on port ' + PORT + '\n');
-    console.log('Database_URL: ' + process.env.DATABASE_URL + '\n');
-});
+  console.log('Server listening on port ' + PORT + '\n');
+  // console.log('Database_URL: ' + process.env.DATABASE_URL + '\n');
+})
 
-
-app.use(express.json());
+app.use(express.json())
 
 //API Calls***************************************************************************************
 //Login
 //Login
-app.get('/status', async (req, res, next) =>{
-    return res.status(200).send('Server is On\n');
+app.get('/status', async (req, res, next) => {
+  return res.status(200).send('Server is On\n')
 })
 
 app.post('/api/login', async (req, res, next) => {
+  // incoming: username, password
+  // outgoing: id, error
+  var error = ''
+  const { username, password, email, googleID } = req.body
+  const input = req.body
 
-    // incoming: username, password
-    // outgoing: id, error
-    var error = '';
-    const { username, password, email, googleID} = req.body;
-    const input = req.body;
+  //If any missing, return 204
+  if (
+    (username == null && email == null) ||
+    (googleID == null && password == null)
+  ) {
+    const er = { error: 'No Content' }
+    console.log(er)
+    return res.status(204).json(er)
+  }
+  //Search database for users with username and password. If so, return UserID
+  try {
+    await client.connect()
+    const db = client.db('COP4331LargeProjectDatabase');
+    const collection = db.collection('account');
+    const cursor = collection.find({username:username, password:password});
+    // const cursor = collection.find({});
+    const result = await cursor.toArray();
 
-    //If any missing, return 204
-    if (username == null || password == null || email == null || googleID == null) {
-        
-        const er = {error: 'No Content'};
-        console.log(er);
-        return res.status(204).json(er);
+    if (result.length > 0) {
+      const id = result[0]._id
+      var ret = { UserID: id, error: '' }
+      return res.status(200).json(ret)
     }
-    //Search database for users with username and password. If so, return UserID
-    // try {
-    //     const db = client.db();
-    //     const results = await
-    //         db.collection('Users').find({ Login: username, Password: password }).toArray();
-    //     var id = -1;
-    //     var fn = '';
-    //     var ln = '';
-    //     if (results.length > 0) {
-    //         id = results[0].UserId;
-    //         var ret = { UserID: id, error: '' };
-    //         return res.status(200).json(ret);
-    //     }
-    //     //If not, return 400 error stating either username or password is wrong
-    //     return res.status(400).json('Username or Password Is Wrong');
-
-    // }
-    // catch (e) {
-    //     return res.status(500).send('Server Error');
-    // }
-    return res.status(200).send(input);
-
-});
+  } finally {
+    await client.close()
+  }
+  return res.status(409).send('Username or Password Wrong')
+})
 
 //Signup
 app.post('/api/signup', async (req, res, next) => {
-    //incoming username, display name, password, googleID, email
-    const { username, display_name, password, googleID, email } = req.body;
-    const input = req.body;
+  //Create user in database & return UserID
+  // try {
+  //     const db = client.db();
+  //     const result = db.collection('Users').insertOne(newUser);
+  //     return res.status(200).send(result);
 
-    //If any missing, return 204
-    if (username == null || display_name == null || password == null || googleID == null || email == null) {
-        const er = {error: 'No Content'};
-        console.log(er);
-        return res.status(204).send('No Content');
+  //     await prisma.account.create({
+  //         data:{
+  //             email: email,
+  //             name: displayName,
+  //             username: username,
+  //             password: password,
+  //             googleId: googleID,
+  //             image: profilePic,
+  //             desc: description
+  //         }
+  //     })
+  // }
+  // catch (e) {
+  //     error = e.toString();
+  //     return res.status(500).send('Server error');
+  // }
+
+  //incoming username, display name, password, googleID, email
+  const { username, display_name, password, googleID, email } = req.body
+  const input = req.body
+
+  //If any missing, return 204
+  if (
+    username == null ||
+    display_name == null ||
+    password == null ||
+    googleID == null ||
+    email == null
+  ) {
+    const er = { error: 'No Content' }
+    console.log(er)
+    return res.status(204).send('No Content')
+  }
+
+  const newUser = {
+    username: username,
+    display_name: display_name,
+    password: password,
+    email: email
+  }
+
+  try {
+    await client.connect()
+
+    const db = client.db('COP4331LargeProjectDatabase')
+    const collection = db.collection('account')
+    const cursor = collection.find({username:username})
+    const result = await cursor.toArray()
+    if (result.length > 0) {
+      return res.status(409).json('Account Exists')
     }
+  } finally {
+    await client.close()
+  }
 
-    //Search for any users with the same username or email
-    // try {
-    //     const db = client.db();
-    //     const results = await
-    //         db.collection('Users').find({ UserName: username }).toArray();
-    //     var id = -1;
-    //     var fn = '';
-    //     var ln = '';
-    //     if (results.length > 0) {
-    //         return res.status(409).json('Account with provided username already exists');
-    //     }
-    //     results = await
-    //         db.collection('Users').find({ Email: email }).toArray();
-    //     if (results.length > 0) {
-    //         return res.status(409).json('Account with provided email already exists');
-    //     }
-    // }
-    // catch (e) {
-    //     return res.status(500).send('Server Error');
-    // }
-
-    const newUser = { UserName: username, DisplayName: display_name, Password: password, GoogleID: googleID, Email: email };
-    //Create user in database & return UserID
-    // try {
-    //     const db = client.db();
-    //     const result = db.collection('Users').insertOne(newUser);
-    //     return res.status(200).send(result);
-
-    //     await prisma.account.create({
-    //         data:{
-    //             email: email,
-    //             name: displayName,
-    //             username: username,
-    //             password: password,
-    //             googleId: googleID,
-    //             image: profilePic,
-    //             desc: description
-    //         }
-    //     })
-    // }
-    // catch (e) {
-    //     error = e.toString();
-    //     return res.status(500).send('Server error');
-    // }
-
-    return res.status(200).send(input);
-});
-
+  try {
+    await client.connect()
+    const db = client.db('COP4331LargeProjectDatabase')
+    const collection = db.collection('account')
+   
+    const result = await collection.insertOne(newUser)
+    const id = result.insertedId
+    console.log(id)
+    var ret = { UserID: id, error: '' }
+    return res.status(200).json(ret)
+  } finally {
+    client.close()
+  }
+})
 
 // //GetProfile
 app.post('/api/getProfile', async(req, res, next) =>{
    
-    const {id, temp} = req.body;
-    
-    const user = await prisma.account.findFirst({
-        where: { id: id }
+    const {id} = req.body;
+    console.log(id+"\n");
+    const user = await prisma.account.findMany({
+        where: { email: "testme@test.com" }
     })
     return user;
 })
