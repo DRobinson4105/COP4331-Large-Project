0de0dde1484f0feb9fb560c92d0645b31b24968c
Bobby <bobbymorris32304@gmail.com>
2024-11-02 22:34:59 -0400

    Added getProfile, added unit testing for getProfile, added documentation, and edited account to have no recipe list

---
+++ api/user/getProfile.js
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    if (req.method !== 'GET') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+
+    try {
+        const { id } = req.body
+
+        if(id == null){
+            return res.status(400).json({
+                error: 'Input must contain an id input'
+            })
+        }
+
+        if (
+            (id && typeof id !== 'string')
+        ) {
+            return res.status(400).json({
+            error: 'Entered id must be a string'
+            })
+        }
+
+        let user = await prisma.account.findFirst({
+            where: {
+            ...(id ? { id } : {})
+            }
+        })
+
+        if (user == null) {
+            return res.status(409).json({ error: 'Account not found' })
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(200).json(user)
+    } catch (error) {
+        console.error('Error during getProfile:', error);
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(500).json({ error: error });
+    } finally {
+        await prisma.$disconnect();
+    }
+}
---
+++ tests/api/user/getProfile.test.js
@@ -0,0 +1,125 @@
+import handler from '../../../api/user/getProfile';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+describe('POST /api/user/getProfile', () => {
+    const request = async (body) => {
+        const req = {
+            method: 'GET',
+            body: body
+        };
+    
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            setHeader: jest.fn()
+        };
+    
+        await handler(req, res);
+
+        return {status: res.status.mock.calls[0][0], body: res.json.mock.calls[0][0]};
+    }
+
+    let test1Id, test2Id, testAccountId;
+
+    beforeAll(async() => {
+        await prisma.$connect();
+        
+        const testImage = './_testPhoto.jpg'
+        const unencoded = btoa(testImage);
+        
+        try {
+            await prisma.account.deleteMany({
+                where: {
+                    username: { startsWith: '_test' }
+                }
+            })
+            await prisma.recipe.deleteMany({
+				where: {
+					name: { startsWith: '_test' }
+				}
+			})
+            testAccountId = await prisma.account.create({
+                data: {
+                    name: "_test1",
+                    desc: "testing desc",
+                    image: unencoded,
+                    email: "_test1@test.com",
+                    username: "testuser",
+                    password: "password",
+                }
+            })
+            testAccountId = testAccountId.id
+
+            test1Id = await prisma.recipe.create({
+                data: {
+                    name: "_test1",
+                    desc: "testing desc",
+                    image: unencoded,
+                    macroTrack: [1.0, 2.0, 3.0, 4.0],//Needs Four 
+                    authorId: testAccountId,
+                    instructions: ["testInstructions"],
+                    ingredients: ["testing"],
+                    tagId: ["6724e84caf5041d082f98234"]//Make tags before recipes to attach
+                }
+            })
+            test1Id = test1Id.id
+            
+        } catch (error) {
+            console.error('Error Deleting Test Entries:', error)
+        }
+    })
+
+    afterAll(async () => {
+        await prisma.$disconnect();
+    });
+
+    it('should take in an id and return the account', async() =>{
+        let account, response, expected;
+
+        account = { id: testAccountId}
+        response = await request(account)
+        expected = await prisma.account.findFirst({
+            where: {id : testAccountId}
+        })
+        expect(response.status).toBe(200)
+        expect(response.body.id).toEqual(expected.id)
+
+    })
+
+    it('should fail/return 400 due to invalid argument', async() =>{
+        let account, response, expected;
+
+        account = { id: 1}
+        response = await request(account)
+        expected = await prisma.account.findFirst({
+            where: {id : testAccountId}
+        })
+        expect(response.status).toBe(400)
+
+    })
+
+    it('should fail/return 400 due to missing argument', async() =>{
+        let account, response, expected;
+
+        account = {}
+        response = await request(account)
+        expected = await prisma.account.findFirst({
+            where: {id : testAccountId}
+        })
+        expect(response.status).toBe(400)
+
+    })
+
+    it('should fail/return 409 due to missing argument', async() =>{
+        let account, response, expected;
+
+        account = {id: test1Id}
+        response = await request(account)
+        expected = await prisma.account.findFirst({
+            where: {id : testAccountId}
+        })
+        expect(response.status).toBe(409)
+    })
+})
