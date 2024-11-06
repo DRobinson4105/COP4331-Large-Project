fd57c8a66d26d4c6fe2568256466535ef50bc2ca
Luccccccc <konvalinalucas@gmail.com>
2024-11-06 16:45:23 -0500

    Updates to profile section

---
+++ src/components/Description.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../index.css";
+
+const Description: React.FC = () => {
+  return (
+    <div className="description">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </div>
+  );
+};
+
+export default Description;
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import VerifiedNavBar from '../components/VerifiedNavBar';
+import Description from '../components/Description';
 import "../index.css";
 
 const recipes = [
   {
     name: 'Steak',
-    image: 'path_to_image', // replace with actual image path or URL
+    image: 'path_to_image', 
     tags: ['Meal Prep', 'Low Calorie', 'Beef'],
     calories: 1000,
     protein: '5g',
@@ -14,14 +15,69 @@ const recipes = [
   },
   {
     name: 'Grilled Chicken',
-    image: 'path_to_image', // replace with actual image path or URL
+    image: 'path_to_image', 
     tags: ['High Protein', 'Low Fat', 'Chicken'],
     calories: 800,
     protein: '35g',
     fat: '10mg',
     carbs: '5mg'
   },
-  // Add more recipes here
+  {
+    name: 'Steak',
+    image: 'path_to_image', 
+    tags: ['Meal Prep', 'Low Calorie', 'Beef'],
+    calories: 1000,
+    protein: '5g',
+    fat: '250mg',
+    carbs: '20mg'
+  },
+  {
+    name: 'Steak',
+    image: 'path_to_image', 
+    tags: ['Meal Prep', 'Low Calorie', 'Beef'],
+    calories: 1000,
+    protein: '5g',
+    fat: '250mg',
+    carbs: '20mg'
+  },
+  {
+    name: 'Steak',
+    image: 'path_to_image', 
+    tags: ['Meal Prep', 'Low Calorie', 'Beef'],
+    calories: 1000,
+    protein: '5g',
+    fat: '250mg',
+    carbs: '20mg'
+  },
+  {
+    name: 'Steak',
+    image: 'path_to_image', 
+    tags: ['Meal Prep', 'Low Calorie', 'Beef'],
+    calories: 1000,
+    protein: '5g',
+    fat: '250mg',
+    carbs: '20mg'
+  },
+  {
+    name: 'Steak',
+    image: 'path_to_image', 
+    tags: ['Meal Prep', 'Low Calorie', 'Beef'],
+    calories: 1000,
+    protein: '5g',
+    fat: '250mg',
+    carbs: '20mg'
+  },
+  {
+    name: 'Steak',
+    image: 'path_to_image', 
+    tags: ['Meal Prep', 'Low Calorie', 'Beef'],
+    calories: 1000,
+    protein: '5g',
+    fat: '250mg',
+    carbs: '20mg'
+  },
+  
+  
 ];
 
 const ProfilePage: React.FC = () => {
@@ -36,37 +92,44 @@ const ProfilePage: React.FC = () => {
       <VerifiedNavBar /> 
       
       <div className="profile-container">
+        {/* Sidebar Section */}
         <div className="sidebar">
           <div className="avatar-placeholder"></div>
           <div className="display-name">Display name</div>
-          <div className="description">Description</div>
+          
+          {/* Description Section */}
+          <Description />
         </div>
-        
-        <div className="recipes-section">
+
+        <div className="main-content">
           <h2 className="recipes-title">Name’s Recipes</h2>
-          <div className="recipes-grid">
-            {recipes.map((recipe, index) => (
-              <div className="recipe-card" key={index}>
-                <img className="recipe-image" src={recipe.image} alt={recipe.name} />
-                <div className="recipe-info">
-                  <h3 className="recipe-name">{recipe.name}</h3>
-                  <div className="tags">
-                    <span>Tags:</span>
-                    <ul>
-                      {recipe.tags.map((tag, idx) => (
-                        <li key={idx}>{tag}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="nutrition">
-                    <p>{recipe.calories} Calories</p>
-                    <p>{recipe.protein} Protein</p>
-                    <p>{recipe.fat} Fat</p>
-                    <p>{recipe.carbs} Carbs</p>
+          
+          {/* Recipes Section */}
+          <div className="recipes-section">
+            <div className="recipes-grid">
+              {recipes.map((recipe, index) => (
+                <div className="recipe-card" key={index}>
+                  <img className="recipe-image" src={recipe.image} alt={recipe.name} />
+                  <div className="recipe-info">
+                    <h3 className="recipe-name">{recipe.name}</h3>
+                    <div className="tags">
+                      <span>Tags:</span>
+                      <ul>
+                        {recipe.tags.map((tag, idx) => (
+                          <li key={idx}>{tag}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className="nutrition">
+                      <p>{recipe.calories} Calories</p>
+                      <p>{recipe.protein} Protein</p>
+                      <p>{recipe.fat} Fat</p>
+                      <p>{recipe.carbs} Carbs</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
