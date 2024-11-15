83edf09b3edf6043022fc625083f1bc7557df3bc
David Robinson <drobinson4105@gmail.com>
2024-11-15 00:08:01 -0500

    Final verify changes

--- api/user/verifyAccount.js
+++ api/user/verifyAccount.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
             })
         }
 
-        let ret = { error: '' }
+        let ret = { error: 'Account validated' }
         res.setHeader('Content-Type', 'application/json');
         return res.status(200).json(ret)
     } catch (error) {
