b44bbbd14781e994d736bfcbb06760645d7e82a6
David Robinson <drobinson4105@gmail.com>
2024-11-14 22:49:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import LandingPage from './pages/Landing'
 import LoginPage from './pages/Login'
 import SignUpPage from './pages/SignUp'
 import ForgotPasswordPage from './pages/ForgotPassword'
-import ResetCodeInputPage from './pages/ResetCodeInput'
-import ResetPasswordPage from './pages/ResetPassword'
 import CompleteProfilePage from './pages/CompleteProfile'
 import ProfilePage from './pages/ProfilePage'
 import SearchPage from './pages/Search'
@@ -20,8 +18,6 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
-        <Route path="/resetcodeinput" element={<ResetCodeInputPage />} />
-        <Route path="/resetpassword" element={<ResetPasswordPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/completeprofile" element={<CompleteProfilePage />} />
         <Route path="/Search" element={<SearchPage />} />
