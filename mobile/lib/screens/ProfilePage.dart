4eed7d885d9d8e6431d12dda3c335f4b678d94aa
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-11-15 12:42:49 -0500

    Changes to ProfilePage
    Added function for API calls. Must test to ensure it works.

--- mobile/lib/screens/ProfilePage.dart
+++ mobile/lib/screens/ProfilePage.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/screens/LogIn.dart';
 import '../widgets/RecipeCard.dart';
+import 'package:http/http.dart' as http;
+
+void getProfilePage(BuildContext context, String id) async {
+  if(id.isEmpty){
+    print("Cannot find user with empty id");
+    return;
+  }
+
+
+  try{
+    http.Response response = await htttp.get(
+      Uri.parse('http://nomnom.network:3000/api/user/get'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String, String> {
+        "id": id,
+      }),
+    );
+
+    if(response.statusCode == 200){
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return ProfilePage();
+      }));
+    }
+    print(response.body);
+    return response;
+  } catch(e){
+    print("Error occured retriving Profile")
+  }
+}
 
 class ProfilePage extends StatelessWidget {
   @override
