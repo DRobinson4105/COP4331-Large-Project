16c239b684b1818baff38b44d3855a72580061fb
David Robinson <drobinson4105@gmail.com>
2024-11-14 23:57:05 -0500

    Update verifyAccount.js

--- api/user/verifyAccount.js
+++ api/user/verifyAccount.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     try {
         const { id, code } = req.body
 	
-        if (email == null || code == null) {
+        if (id == null || code == null) {
             return res.status(400).json({
                 error: 'Invalid account'
             })
