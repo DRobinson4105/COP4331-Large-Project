e178e0b1752d4e3cbdce94ba0698324d85ff6d56
Lucas K <konvalinalucas@gmail.com>
2024-11-14 23:38:53 -0500

    create recipe

--- api/server.js
+++ api/server.js
@@ -21,11 +21,12 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: (origin, callback) => callback(null, true),
-  methods: ['GET', 'POST', 'OPTIONS'],
+  origin: ['http://localhost:5173', '*'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
 };
+
 
 app.use(cors(corsOptions));
 
