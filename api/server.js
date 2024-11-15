613d78e9a87ffd66c253cf841c76915c1252d3c8
David Robinson <drobinson4105@gmail.com>
2024-11-14 23:54:43 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
