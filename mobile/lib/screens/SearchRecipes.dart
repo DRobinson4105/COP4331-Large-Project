9158434b5d5c979fd10d170ae419ffec54e6e76e
Jake Wright <jpw306@outlook.com>
2024-11-19 23:37:33 -0500

    Removed print functions

--- mobile/lib/screens/SearchRecipes.dart
+++ mobile/lib/screens/SearchRecipes.dart
@@ -33,16 +33,6 @@ class SearchRecipesState extends State<SearchRecipes>{
   int maxCarbs = 1000000;
 
   Future fetchRecipes(String name, List<dynamic> tagId, int minCalories, maxCalories, minProtein, maxProtein, minFat, maxFat, minCarbs, maxCarbs) async {
-    print(name);
-    print(tagId);
-    print(minCalories);
-    print(maxCalories);
-    print(minProtein);
-    print(maxProtein);
-    print(minFat);
-    print(maxFat);
-    print(minCarbs);
-    print(maxCarbs);
     try {
       http.Response response = await http.post(
         Uri.parse('http://nomnom.network:3000/api/recipe/search'),
@@ -66,7 +56,6 @@ class SearchRecipesState extends State<SearchRecipes>{
       if (response.statusCode == 200) {
         // If the server did return a 200 OK response,
         // then parse the JSON.
-        print(response.body);
         return json.decode(response.body);
       } else {
         // If the server did not return a 200 OK response,
@@ -91,8 +80,6 @@ class SearchRecipesState extends State<SearchRecipes>{
     this.maxFat = maxFat;
     this.minCarbs = minCarbs;
     this.maxCarbs = maxCarbs;
-
-    print(name);
 
     fetchRecipes(name, tagId, minCalories, maxCalories, minProtein, maxProtein, minFat, maxFat, minCarbs, maxCarbs).then((value) async {
       recipeController.add(value);
